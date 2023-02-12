2e4c9299c38fc4fa4650ccf65c71ad35cec1a1ed
Nazim <nazim86@gmail.com>
2023-02-12 20:33:39 +0400

    last question on 1st week exam

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
 import 'dotenv/config'
-import express, {Request, Response, NextFunction} from 'express'
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
-// create express app
 const app = express()
 
-const port = process.env.PORT
+app.use(bodyParser.json())
 
-app.use('/', (req: Request, res: Response) => {
-    res.send("WORKS")
+app.get('/news/categories', (req: Request, res: Response) => {
+    res.send(newsCategories.map(c => ({ title: c.title })))
+    // console.log(newsCategories.map(c => ({ title: c.title })))
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port: ${port}`)
+app.delete('/news/categories/:id', (req: Request, res: Response) => {
+    newsCategories = newsCategories.filter(c => c.id !== +req.params.id)
+    res.send(204)
 })
 
+app.put('/news/categories/:id', (req: Request, res: Response) => {
+    for (const c of newsCategories) {
+        if (c.id === +req.params.id) {
+            c.title = req.body.title
+            break;
+        }
+    }
+    res.send(204)
+})
+
+app.get('/news/categories/:id', (req: Request, res: Response) => {
+    res.send(newsCategories.find(c => c.id === +req.params.id))
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port: ${process.env.PORT}`)
+})
+let newsCategories = [
+    {id: 1, title: 'sport'},
+    {id: 2, title: 'politics'},
+    {id: 3, title: 'economic'}
+]
